04ed529026157a6f611364bd48d8b35d93e517b0
Bernard Karam <bernard.karam@real-digital-agency.de>
2019-06-20 00:10:28 +0200

    creation

---
+++ app/src/main/java/bernardo/bernardinhio/rssfeedsonerecyclerviewretrofit/MainActivity.kt
@@ -0,0 +1,25 @@
+package bernardo.bernardinhio.rssfeedsonerecyclerviewretrofit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * https://www.journaldev.com/20126/android-rss-feed-app
+ * https://www.androidhive.info/2012/05/android-rss-reader-application-using-sqlite-part-1/
+ * https://www.androidhive.info/2012/05/android-rss-reader-application-using-sqlite-part-2/
+ * https://www.youtube.com/watch?v=wSgAVnuYkjk
+ * https://www.youtube.com/watch?v=APInjVO0WkQ
+ * https://code.tutsplus.com/tutorials/build-an-rss-20-feed-with-codeigniter--net-7811
+ * https://www.androidauthority.com/simple-rss-reader-full-tutorial-733245/
+ * https://www.androidauthority.com/simple-rss-reader-full-tutorial-733245/
+ * https://medium.com/@marcogomiero/how-to-easily-handle-rss-feeds-on-android-with-rss-parser-8acc98e8926f
+ *
+ */
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
